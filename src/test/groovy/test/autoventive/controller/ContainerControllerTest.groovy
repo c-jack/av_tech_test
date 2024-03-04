af65999616a077e8e684ce384cbb4041154cb94a
r3za-G <119519070+r3za-G@users.noreply.github.com>
2024-03-04 11:07:01 +0000

    Update ContainerControllerTest.groovy

--- src/test/groovy/test/autoventive/controller/ContainerControllerTest.groovy
+++ src/test/groovy/test/autoventive/controller/ContainerControllerTest.groovy
@@ -27,19 +27,30 @@ class ContainerControllerTest extends Specification {
     }
 
     /**
-     * Test for {@link ContainerController#list}
-     */
+    * Test for {@link ContainerController#list}
+    */
     def "list should return all persisted Containers"() {
-        given: "I create two distinct containers"
+        given: "I create two distinct containers with different statuses"
         def container1 = new Container(identifier: "Container1", capacity: 10, status: Status.AVAILABLE)
         def container2 = new Container(identifier: "Container2", capacity: 10, status: Status.OUT_OF_SERVICE)
 
         and: "I stub the getAll DAO method to return the containers"
         mockContainerDao.getAll() >> [container1, container2]
 
-        expect: "list to return the value retrieved from the DAO"
-        controller.list() == [container1, container2]
+        expect: "list to return all containers when no status is provided"
+        controller.list(null) == [container1, container2]
 
+        and: "I stub the getAll DAO method to return only containers with status AVAILABLE"
+        mockContainerDao.getAll() >> [container1]
+
+        expect: "list to return containers with status AVAILABLE when status AVAILABLE is provided"
+        controller.list(Status.AVAILABLE) == [container1]
+
+        and: "I stub the getAll DAO method to return only containers with status OUT_OF_SERVICE"
+        mockContainerDao.getAll() >> [container2]
+
+        expect: "list to return containers with status OUT_OF_SERVICE when status OUT_OF_SERVICE is provided"
+        controller.list(Status.OUT_OF_SERVICE) == [container2]
     }
 
     /**
@@ -74,5 +85,20 @@ class ContainerControllerTest extends Specification {
 
         then: "The DAO Save method should be invoked once with the provided object"
         1 * mockContainerDao.save(container)
+    }
+
+    /**
+     * Test for {@link ContainerController#delete}
+     */
+    def "delete should delete the container when it exists"() {
+        given: "a container with the specified identifier exists"
+        def identifier = "Container"
+        
+
+        when: "delete method is called with the existing identifier"
+        controller.delete(identifier)
+
+        then: "The DAO Delete method should be invoked once with the provided object"
+        1 * mockContainerDao.delete(identifier)
     }
 }
