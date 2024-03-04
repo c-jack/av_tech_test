2a6861f61fa99937e8857bab6d7f5aef62076e67
r3za-G <119519070+r3za-G@users.noreply.github.com>
2024-03-04 11:06:12 +0000

    Create Orientation.groovy

---
+++ src/main/groovy/test/autoventive/enums/Orientation.groovy
@@ -0,0 +1,16 @@
+package test.autoventive.enums
+
+/**
+ * Container Orientation
+ */
+enum Orientation {
+    /**
+     * Container is facing forwards
+     */
+    FORWARDS,
+
+    /**
+     * Container is facing backwards
+     */
+    BACKWARDS;
+}
